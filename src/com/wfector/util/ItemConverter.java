60ba0ed11c9e4ba375d2020c3c589e875c5b7a14
Max Lee <Phoenix616@users.noreply.github.com>
2014-08-19 17:26:00 +0200

    Renamed variable itemId to itemName

--- src/com/wfector/util/ItemConverter.java
+++ src/com/wfector/util/ItemConverter.java
@@ -2,9 +2,9 @@ package com.wfector.util;
 
 public class ItemConverter {
 
-	public static String GetItemName(String itemId) {
+	public static String GetItemName(String itemName) {
 		
-		return itemId;
+		return itemName;
 		
 	}
 }
