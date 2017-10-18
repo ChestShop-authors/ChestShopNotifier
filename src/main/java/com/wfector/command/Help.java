7e0534cbdd404d3713a4246eefb6413d0c3e43d3
FrankHeijden <22407829+FrankHeijden@users.noreply.github.com>
2017-10-18 20:23:59 +0200

    Update Help.java
    + Translations

--- src/main/java/com/wfector/command/Help.java
+++ src/main/java/com/wfector/command/Help.java
@@ -2,48 +2,61 @@ package com.wfector.command;
 
 import java.util.ArrayList;
 
+import com.wfector.notifier.ChestShopNotifier;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class Help {
+    private static ChestShopNotifier plugin;
+
+    public Help(ChestShopNotifier main) {
+        plugin = main;
+    }
 
     public static void SendDialog(CommandSender sender) {
 
         ArrayList<String> helpItems = new ArrayList<String>();
-
-        helpItems.add("&dChestShop Notifier // &7Commands");
-        helpItems.add("&c ");
-        helpItems.add("&7 /csn &dhelp &f- Plugin usage & commands");
-        if (sender.hasPermission("csn.command.history")) {
-            helpItems.add("&7 /csn &dhistory [<page>]&f- View sales");
+        if(plugin.getMessage("help.header") != null) {
+            helpItems.add(plugin.getMessage("help.header"));
         }
-        if (sender.hasPermission("csn.command.read")) {
-            helpItems.add("&7 /csn &dread &f- Mark all sales as read");
+        if(plugin.getMessage("help.help") != null) {
+            helpItems.add(plugin.getMessage("help.help"));
         }
-        if (sender.hasPermission("csn.command.clear")) {
-            helpItems.add("&7 /csn &dclear &f- Remove read sales");
+        if(sender.hasPermission("csn.command.history") && plugin.getMessage("help.history") != null) {
+            helpItems.add(plugin.getMessage("help.history"));
         }
-        helpItems.add("&c ");
-
-        if (sender.hasPermission("csn.command.history.others")) {
-            helpItems.add("&7 /csn &dhistory <username> [<page>]&f- View sales of another player");
+        if(sender.hasPermission("csn.command.read") && plugin.getMessage("help.read") != null) {
+            helpItems.add(plugin.getMessage("help.read"));
         }
-        if (sender.hasPermission("csn.command.cleandatabase")) {
-            helpItems.add("&7 /csn &ccleandatabase &f- Remove database entries. Parameters:");
-            helpItems.add("&c --older-than, -o <days> &f- Removes entries older than <days>");
-            helpItems.add("&c --user, -user <username/uuid> &f- Removes entries from a single user only");
-            helpItems.add("&c --read-only, -r &f- Removes only read entries, default behaviour");
-            helpItems.add("&c --all, -a &f- Removes all entries");
+        if(sender.hasPermission("csn.command.clear") && plugin.getMessage("help.clear") != null) {
+            helpItems.add(plugin.getMessage("help.clear"));
         }
-        if (sender.hasPermission("csn.command.upload")) {
-            helpItems.add("&7 /csn &cupload &f- Force update databases");
+        if(sender.hasPermission("csn.command.history.others") && plugin.getMessage("help.history-others") != null) {
+            helpItems.add(plugin.getMessage("help.history-others"));
         }
-        if (sender.hasPermission("csn.command.convert")) {
-            helpItems.add("&7 /csn &cconvert &f- Convert database to UUIDs");
+        if(sender.hasPermission("csn.command.cleandatabase") && plugin.getMessage("help.cleandatabase") != null) {
+            helpItems.add(plugin.getMessage("help.cleandatabase"));
         }
-
-        if (sender.hasPermission("csn.command.reload")) {
-            helpItems.add("&7 /csn &creload &f- Reload configuration");
+        if(sender.hasPermission("csn.command.cleandatabase") && plugin.getMessage("help.cleandatabase-older-than") != null) {
+            helpItems.add(plugin.getMessage("help.cleandatabase-older-than"));
+        }
+        if(sender.hasPermission("csn.command.cleandatabase") && plugin.getMessage("help.cleandatabase-user") != null) {
+            helpItems.add(plugin.getMessage("help.cleandatabase-user"));
+        }
+        if(sender.hasPermission("csn.command.cleandatabase") && plugin.getMessage("help.cleandatabase-read-only") != null) {
+            helpItems.add(plugin.getMessage("help.cleandatabase-read-only"));
+        }
+        if(sender.hasPermission("csn.command.cleandatabase") && plugin.getMessage("help.cleandatabase-all") != null) {
+            helpItems.add(plugin.getMessage("help.cleandatabase-all"));
+        }
+        if(sender.hasPermission("csn.command.upload") && plugin.getMessage("help.upload") != null) {
+            helpItems.add(plugin.getMessage("help.upload"));
+        }
+        if(sender.hasPermission("csn.command.convert") && plugin.getMessage("help.convert") != null) {
+            helpItems.add(plugin.getMessage("help.convert"));
+        }
+        if(sender.hasPermission("csn.command.reload") && plugin.getMessage("help.reload") != null) {
+            helpItems.add(plugin.getMessage("help.reload"));
         }
 
         for(String item : helpItems) {
