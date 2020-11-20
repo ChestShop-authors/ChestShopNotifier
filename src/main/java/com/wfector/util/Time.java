3857af5212c26953ee5de8a4ec28bf4c96681d5f
Mareon <movimakers@seznam.cz>
2020-11-20 12:16:32 +0100

    Update Time.java
    Some format changes for more user-friendly looking.

--- src/main/java/com/wfector/util/Time.java
+++ src/main/java/com/wfector/util/Time.java
@@ -17,24 +17,24 @@ public class Time {
 
         if(Math.floor(difference / 86400) > 0) {
             int days = (int) Math.floor(difference / 86400);
-            timestampStr.append(days).append("d");
+            timestampStr.append(days).append("d ");
             difference = (difference - (days * 86400));
         }
 
         if(Math.floor(difference / 3600) > 0) {
             int hours = (int) Math.floor(difference / 3600);
-            timestampStr.append(hours).append("h");
+            timestampStr.append(hours).append("h ");
             difference = (difference - (hours * 3600));
         }
 
         if(Math.floor(difference / 60) > 0) {
             int minutes = (int) Math.floor(difference / 60);
-            timestampStr.append(minutes).append("m");
+            timestampStr.append(minutes).append("m ");
             difference = (difference - (minutes * 60));
         }
 
         if(timestampStr.length() == 0) {
-            timestampStr.append(difference).append("s");
+            timestampStr.append(difference).append("s ");
         }
 
         return timestampStr.toString();
